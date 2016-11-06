0e74ae7a2c18ca9d60f2deaa63f48a968d3d7ce0
paul <paul@cueb.io>
2016-11-06 00:32:18 -0700

    checkpoint

---
+++ src/component/arrowManager.js
@@ -0,0 +1,83 @@
+
+const ACC_THRESHOLD = 1;
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+function getRandomDir() {
+  const num = Math.random() * 10;
+  if (num < 2.5) {
+	  return 'UP';
+  } else if (num < 5) {
+	  return 'DOWN';
+  } else if (num < 7.5) {
+    return 'LEFT';
+  } else {
+    return 'RIGHT';
+  }
+}
+
+function getArrowSpawnParams() {
+  return `color: ${getRandomColor()}; direction: ${getRandomDir()}`;
+}
+
+AFRAME.registerComponent('arrowManager', {
+  schema: {
+    enabled: {
+      type: 'bool',
+      default: true,
+    },
+    spawnFreqMs: {
+      type: 'int',
+      default: 2500,
+    }
+  },
+  // lifecycle
+  init() {
+    this._lastTime = null;
+    this._arrows = [];
+
+    // register motion detector
+		//this._deviceMotion = FULLTILT.getDeviceMotion();
+
+		//deviceMotion.then(motionData => {
+			//motionData.listen(function() {
+				//// Display calculated screen-adjusted devicemotion
+				//const screenAcc = motionData.getScreenAdjustedAcceleration() || {};
+				//const screenAccG = motionData.getScreenAdjustedAccelerationIncludingGravity() || {};
+				//const screenRotRate = motionData.getScreenAdjustedRotationRate() || {};
+
+        //console.info('ScreenAcc.X::', screenAcc.x);
+        //console.info('ScreenAcc.Y::', screenAcc.y);
+        //console.info('ScreenAcc.Z::', screenAcc.z);
+
+        //console.info('ScreenAccG.X::', screenAccG.x);
+        //console.info('ScreenAccG.Y::', screenAccG.y);
+        //console.info('ScreenAccG.Z::', screenAccG.z);
+			//});
+		//});
+  },
+
+  _spawnArrow() {
+    const entity = document.createElement('a-entity');
+
+    this._arrows.push(entity);
+
+    const idCounter = this._arrows.length;
+
+    entity.setAttribute('arrow', getArrowSpawnParams());
+    entity.setAttribute('id', `entity-${idCounter}`);
+
+    this.el.appendChild(entity);
+  },
+  tick(time, timeDelta) {
+    if (!this._lastTime) {
+      this._lastTime = time;
+    }
+    if (this.data.enabled && (this._lastTime - time) > this.data.spawnFreqMs) {
+      console.info('Generating arrow');
+      this._lastTime = time
+      this._spawnArrow();
+    }
+  },
+});
